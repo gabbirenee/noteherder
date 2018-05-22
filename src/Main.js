44362dd1efb8941297f306407dfeb2c0772befe6
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-22 11:02:42 -0400

    Use a stateless functional component

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-class Main extends React.Component {
-    render () {
-        return (
-            <div className="Main">
-                MAIN
-            </div>
-        )
-    }
+const Main = () => {
+    return ( 
+        <div className="Main">Main</div>
+    )
 }
 
 export default Main
