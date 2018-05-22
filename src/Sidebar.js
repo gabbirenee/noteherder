d563ed7d36a630661bd358958e0b14b955fee999
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-22 11:44:54 -0400

    Import images and add JSX for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
 const Sidebar = () => {
     return (
-        <div className="Sidebar">Sidebar</div>
+        <div className="Sidebar">
+            <div className="Logo">
+                <img src={quill} alt="Noteherder" />
+            </div>
+            
+            <a href="/ntoes">
+                <img src={newHover} alt="New note" />
+                <img src={newIcon} alt="New note" />
+            </a>
+
+            <div class="SignOut">
+                <button>
+                    <i className="fa fa-sign-out"></i>
+                </button>
+            </div>
+        </div>
     )
 }
 
