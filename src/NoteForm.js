c188cdfa273ee3664baec6a829f4cd603c142abb
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-23 11:09:34 -0400

    Use destructuring assignment on props

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,27 +2,31 @@ import React from 'react'
 
 import './NoteForm.css'
 
-const NoteForm = () => {
-    return (
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="far fa-trash-alt"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-              />
-            </p>
-            
-            <textarea name="body"></textarea>
-          </form>
-        </div>
-    )
+const NoteForm = ({currentNote}) => {
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+            value={currentNote.title}
+          />
+        </p>
+
+        <textarea
+          name="body"
+          value={currentNote.body}
+        ></textarea>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
