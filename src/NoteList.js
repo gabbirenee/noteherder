4e129cc03306d20d04f80072867289a02dc13a0a
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-23 09:15:01 -0400

    Continue styling sidebar

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,7 +2,39 @@ import React from 'react'
 
 const NoteList = () => {
     return (
-        <div className="NoteList">NoteList</div>
+        <div className="NoteList">
+            <h3>Notes</h3>
+            <ul id="notes">
+                <a className="active">
+                <li>
+                    <div className="note">
+                    <div className="note-title">
+                        Kohlrabi welsh
+                    </div>
+                    <div className="note-body">
+                        <p>
+                        Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+                        </p>
+                    </div>
+                    </div>
+                </li>
+                </a>
+                <a>
+                <li>
+                    <div className="note">
+                    <div className="note-title">
+                        Dandelion cucumber
+                    </div>
+                    <div className="note-body">
+                        <p>
+                        Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+                        </p>
+                    </div>
+                    </div>
+                </li>
+                </a>
+            </ul>
+        </div>
     )
 }
 
