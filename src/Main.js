fbfca97cea4b273eadc4d463b0daa0617ca0c414
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-23 11:54:05 -0400

    Save notes from the form

--- src/Main.js
+++ src/Main.js
@@ -9,32 +9,16 @@ class Main extends React.Component {
     super()
     this.state = {
       currentNote: this.blankNote(),
-      notes: [
-        {
-          id: 1,
-          title: 'Why I <3 JS',
-          body: 'Because I like code, and JS is code.',
-        },
-        {
-          id: 2,
-          title: 'Thoughts on breakfast',
-          body: 'I love it!',
-        },
-        {
-          id: 3,
-          title: 'Watching the first episode of Black Mirror with your parents',
-          body: 'Don\'t.',
-        },
-      ]
+      notes: [],
     }
   }
 
-  blankNote = () =>{
-      return {
-          id: null,
-          title: '',
-          body: '',
-      }
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
   }
 
   setCurrentNote = (note) => {
@@ -42,18 +26,38 @@ class Main extends React.Component {
   }
 
   resetCurrentNote = () => {
-      this.setCurrentNote(this.blankNote())
+    this.setCurrentNote(this.blankNote())
+  }
+
+  saveNote = (note) => {
+    const notes = [...this.state.notes]
+
+    if (!note.id) {
+      // new note
+      note.id = Date.now()
+      notes.push(note)
+    } else {
+      // existing note
+      const i = notes.findIndex(currentNote => currentNote.id === note.id)
+      notes[i] = note
+    }
+
+    this.setState({ notes })
+    this.setCurrentNote(note)
   }
 
   render() {
     return (
       <div className="Main" style={style}>
-        <Sidebar resetCurrentNote={this.resetCurrentNote}/>
+        <Sidebar resetCurrentNote={this.resetCurrentNote} />
         <NoteList
           notes={this.state.notes}
           setCurrentNote={this.setCurrentNote}
         />
-        <NoteForm currentNote={this.state.currentNote} />
+        <NoteForm
+          currentNote={this.state.currentNote}
+          saveNote={this.saveNote}
+        />
       </div>
     )
   }
